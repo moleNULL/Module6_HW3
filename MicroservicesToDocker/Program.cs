b4f71182838c77941ae99dc2900e713de01ee5b4
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-02-02 20:29:42 +0200

    Add A-Level eShop_Sample3 to project

--- MicroservicesToDocker/Program.cs
+++ MicroservicesToDocker/Program.cs
@@ -1,20 +1,65 @@
-namespace MicroservicesToDocker
+using Catalog.Host.Configurations;
+using Catalog.Host.Data;
+using Catalog.Host.Repositories;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services;
+using Catalog.Host.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+var configuration = GetConfiguration();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.Configure<CatalogConfig>(configuration);
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddTransient<ICatalogItemRepository, CatalogItemRepository>();
+builder.Services.AddTransient<ICatalogService, CatalogService>();
+builder.Services.AddTransient<ICatalogItemService, CatalogItemService>();
+
+builder.Services.AddDbContextFactory<ApplicationDbContext>(opts => opts.UseNpgsql(configuration["ConnectionString"]));
+builder.Services.AddScoped<IDbContextWrapper<ApplicationDbContext>, DbContextWrapper<ApplicationDbContext>>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
-    public class Program
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapControllers();
+});
+
+CreateDbIfNotExists(app);
+app.Run();
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
+}
+
+void CreateDbIfNotExists(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
     {
-        public static void Main(string[] args)
+        var services = scope.ServiceProvider;
+        try
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var context = services.GetRequiredService<ApplicationDbContext>();
 
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            DbInitializer.Initialize(context).Wait();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
         }
     }
 }
